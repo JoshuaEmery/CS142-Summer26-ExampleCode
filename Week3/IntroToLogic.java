b29a1a83c918ba835051052999e34fac810c3cc3
Joshua Emery <38080740+JoshuaEmery@users.noreply.github.com>
2026-07-21 11:32:15 -0700

    Refactor IntroToLogic and add LogicalOperators class

--- Week3/IntroToLogic.java
+++ Week3/IntroToLogic.java
@@ -1,82 +1,47 @@
 
-public class IntroToLogic {
+public class LogicalOperators {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int x = 20;
-		int y = 10;
+		//These combine two boolean values into one
+		// && Logical and - both values must be true to get a true
+		// || Logical or - both values must be false to get a false
 		
-		//Operators that make a comparison between values and evaulate
-		//to either true or false
-		// == Equality (This is not assignment)
-		// != Not Equal, The ! by itself can be read as not or the opposite of
-		// > Greater Than
-		// >= GTE
-		// < Less Than
-		// <= LTE
-		
-		//The operators can be used to create boolean variables
-		boolean b1 = x == y;
-		boolean b2 = x > y;
-		System.out.println("x == y: " + b1);
-		System.out.println("x > y: " + b2);
-		//It is also possible to embed the boolean statement
-		System.out.println("X not equal y: " + (x != y));
-		
-		//if(condition)  - condition is simply a true or false statement
-		//after the if we make a code block. that code block will only run
-		//when the condition is true
-		if(1 > 0) {
-			System.out.println("This code block always runs");
-		}
-		//when an else block is attached to an if, we get a code block that runs
-		//when the condition is false
-		double temperature = 80;
-		if(temperature > 75) {
-			System.out.println("You dont need a jacket");
+		int x = 10;
+		if(x > 0 && x % 2 == 0) {
+			System.out.println("x is positive and even");
 		}
 		else {
-			System.out.println("You need a jacket");
+			System.out.println("Here I dont know which caused it to fail");
 		}
-		//we need to handle a number being positive or negative
-		int number = 0;
-		if(number < 0) {
-			System.out.println("less than zero: " + number);
-		}
-		else if(number > 0){
-			System.out.println("greater than zero: " + number);
+		
+		if(x > 0 || x % 2 == 0) {
+			System.out.println("Here I dont know which caused it to pass");
 		}
 		else {
-			//what do I know logically if I make it down here
-			System.out.println("The number is zero: " + number);
+			//Here I know both conditions failed
+			System.out.println("x is not greater than 0, x is not even");
 		}
-		//lets see if we can combine our example here odd/even and positive negative
-		//given number I want to know if it is pos and even, pos and odd, negative and even
-		//or negative and odd, 0 is always even
-		if(number == 0) {
-			System.out.println("zero and even");
-		}
-		else if(number > 0) {
-			//if we make it here we know number is positive
-			if(number % 2 == 0) {
-				//here we know number is even and positive
-				System.out.println("pos and even");
-			}
-			else{
-				//what do I know logically here?
-				//number is odd and positive
-				System.out.println("pos and odd");
-			}
-		}
-		else {
-			//If i hit this code block I know number is negative
-			if(number % 2 == 0) {
-				System.out.println("neg and even");
-			}
-			else {
-				System.out.println("neg and odd");
-			}
-		}
+		//if have a statement using && || and i need the opposite of that statement
+		//I flip the operator
+		//De Morgans Law
+		//(x == 0 || y == 0) -> (x != 0 && y != 0)
+		
+		//1 create a variable named age, give it a value
+		//check the age variable and print you are eligible to vote when age >= 18
+		//not eligible otherwise
+		
+		//2: create a variable and give it a value
+		//write a condition that checks if the number is in the range 1-20 (include 1 and 20)
+		//output that the number is in range or not
+		
+		//3: create a variable for a grade on an assignment 0-100 (include 0 and 100)
+		//check if that grade is valid or invalid. A grade is invalid if it is not in the range
+		//-5 should fail, 80 should pass, 110 should fail
+		
+		//4: x and y are coordinates on a cartesian plane. write a decision structure which
+		//determine what quadrant the pair lie in, also consider the point could be at origin
+		//or on the x / y axis
 		
 	}
 
