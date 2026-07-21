689e4199371d2f9927381b7022da3479f76e6f0d
Joshua Emery <38080740+JoshuaEmery@users.noreply.github.com>
2026-07-21 11:33:51 -0700

    Implement logical operators examples in LogicalOperators
    Added examples of logical operators and conditions in main method.

--- Week3/LogicalOperators.java
+++ Week3/LogicalOperators.java
@@ -3,7 +3,46 @@ public class LogicalOperators {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		//These combine two boolean values into one
+		// && Logical and - both values must be true to get a true
+		// || Logical or - both values must be false to get a false
+		
+		int x = 10;
+		if(x > 0 && x % 2 == 0) {
+			System.out.println("x is positive and even");
+		}
+		else {
+			System.out.println("Here I dont know which caused it to fail");
+		}
+		
+		if(x > 0 || x % 2 == 0) {
+			System.out.println("Here I dont know which caused it to pass");
+		}
+		else {
+			//Here I know both conditions failed
+			System.out.println("x is not greater than 0, x is not even");
+		}
+		//if have a statement using && || and i need the opposite of that statement
+		//I flip the operator
+		//De Morgans Law
+		//(x == 0 || y == 0) -> (x != 0 && y != 0)
+		
+		//1 create a variable named age, give it a value
+		//check the age variable and print you are eligible to vote when age >= 18
+		//not eligible otherwise
+		
+		//2: create a variable and give it a value
+		//write a condition that checks if the number is in the range 1-20 (include 1 and 20)
+		//output that the number is in range or not
+		
+		//3: create a variable for a grade on an assignment 0-100 (include 0 and 100)
+		//check if that grade is valid or invalid. A grade is invalid if it is not in the range
+		//-5 should fail, 80 should pass, 110 should fail
+		
+		//4: x and y are coordinates on a cartesian plane. write a decision structure which
+		//determine what quadrant the pair lie in, also consider the point could be at origin
+		//or on the x / y axis
+		
 	}
 
 }
