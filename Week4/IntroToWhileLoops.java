35d4e34e25062f95040665241b1c5e0b727e8cc9
Joshua Emery <38080740+JoshuaEmery@users.noreply.github.com>
2026-07-28 11:34:40 -0700

    Enhance IntroToWhileLoops with more examples
    Added additional while loops to demonstrate counting down, summing positive integers, and printing even numbers.

--- Week4/IntroToWhileLoops.java
+++ Week4/IntroToWhileLoops.java
@@ -31,9 +31,32 @@ public class IntroToWhileLoops {
 		//ask the following, where do I want this variable to start
 		//How do I want it to change
 		//When do I want to stop
+		int count2 = 10;
+		while(count2 > 0) {
+			System.out.println("Count 2: " + count2);
+			count2--;
+		}
+		//Create a variable start and choose a value for it
+		//we are going to add up all positive whole numbers
+		//between 1 and start (include both 1 and start)
+		//output the total, for example start = 5 should output 15
+		int start = 5;
+		int total = 0;
+		while(start > 0) {
+			//System.out.println("Start: " + start);
+			//total = total + start;
+			total += start;
+			start--;
+		}		
+		//when we exit the loop we have calculated the total
+		System.out.println("Total: " + total);
+		
+		//Lets create loop with a variable that starts at 10 and
+		//goes to 20. Include both 10 and 20
+		//Print each even number to the console
+		//Add each odd number to a total
 		
 		
-
 	}
 
 }
