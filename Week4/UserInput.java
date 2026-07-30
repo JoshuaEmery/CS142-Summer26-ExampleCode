5212b36bc3efe49ab56e50a7793c551e5568d476
Joshua Emery <38080740+JoshuaEmery@users.noreply.github.com>
2026-07-30 10:53:08 -0700

    Add files via upload

---
+++ Week4/UserInput.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class UserInput {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//To get input from the user you need a scanner object
+		Scanner scanner = new Scanner(System.in);
+		//We need a prompt (give the user instructions)
+		System.out.println("Please enter your age");
+		//we use the scanner object to capture a string
+		String ageString = scanner.nextLine();
+		//if you need a number, you must parse the string (convert from string to number)
+		int age = Integer.parseInt(ageString);
+		if(age >= 18) {
+			System.out.println("Eligible to vote");
+		}
+		else {
+			System.out.println("Ineligible to vote");
+		}
+		
+		
+
+	}
+
+}
