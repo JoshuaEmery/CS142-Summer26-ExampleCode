f369123082e0a33010e47e91d6ce3803bd21533b
Joshua Emery <38080740+JoshuaEmery@users.noreply.github.com>
2026-07-30 11:04:55 -0700

    Update UserInput.java

--- Week4/UserInput.java
+++ Week4/UserInput.java
@@ -4,24 +4,34 @@ public class UserInput {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		//To get input from the user you need a scanner object
+
+		// To get input from the user you need a scanner object
 		Scanner scanner = new Scanner(System.in);
-		//We need a prompt (give the user instructions)
+		// We need a prompt (give the user instructions)
 		System.out.println("Please enter your age");
-		//we use the scanner object to capture a string
+		// we use the scanner object to capture a string
 		String ageString = scanner.nextLine();
-		//if you need a number, you must parse the string (convert from string to number)
+		// if you need a number, you must parse the string (convert from string to
+		// number)
 		int age = Integer.parseInt(ageString);
-		if(age >= 18) {
+		if (age >= 18) {
 			System.out.println("Eligible to vote");
-		}
-		else {
+		} else {
 			System.out.println("Ineligible to vote");
 		}
-		
-		
-
+	}
+	//lets make a method that will TRY to parse a string, if successful we will return
+	//the number. if not successful we will return null
+	static Double tryParseDouble(String input) {
+		//we can run code that might cause an exception with a try/catch
+		try {
+			Double x = Double.parseDouble(input);
+			//If I make it to this line, I know the parse worked
+			return x;
+		}
+		catch(Exception ex) {
+			return null;
+		}
 	}
 
 }
