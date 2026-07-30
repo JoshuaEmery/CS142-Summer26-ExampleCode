d38a70bee292d30e12f80e73a06694ba6ed8a22d
Joshua Emery <38080740+JoshuaEmery@users.noreply.github.com>
2026-07-30 12:24:06 -0700

    Update ForLoop.java

--- Week4/ForLoop.java
+++ Week4/ForLoop.java
@@ -3,33 +3,82 @@ public class ForLoop {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//For loop is really just a shortcut for a while loop
-		//with a counter that changes
-		//for(st1; st2; st3){}
-		//st1 - runs once before the loop starts (setup variables)
-		//st2 - condition that is checked before each iteration
-		//st3 - runs at the end of each iteration of the loop (change variables)
-		//for loop that runs x times
-		//for(int i = 0; i < x; i++)
-		for(int i = 0; i < 10; i++) {
+		// For loop is really just a shortcut for a while loop
+		// with a counter that changes
+		// for(st1; st2; st3){}
+		// st1 - runs once before the loop starts (setup variables)
+		// st2 - condition that is checked before each iteration
+		// st3 - runs at the end of each iteration of the loop (change variables)
+		// for loop that runs x times
+		// for(int i = 0; i < x; i++)
+		for (int i = 0; i < 10; i++) {
 			System.out.println(i);
 		}
-		//if i is declared in the for loop it is out of scope 
-		//after the for loop
-		//System.out.println(i);
+		// if i is declared in the for loop it is out of scope
+		// after the for loop
+		// System.out.println(i);
 		System.out.println("----Nested Loops----");
-		//When one loop is nested in another, the inner loop
-		//runs in its entirety for each iteration of the outer loop
-		for(int i = 0; i < 4; i++) {
-			
-			for(int j = 0; j < 5; j++) {
-				//This inner loop will run 20 (5 * 4)
-				//The inner loops does the actual work
+		// When one loop is nested in another, the inner loop
+		// runs in its entirety for each iteration of the outer loop
+		for (int i = 0; i < 4; i++) {
+
+			for (int j = 0; j < 5; j++) {
+				// This inner loop will run 20 (5 * 4)
+				// The inner loops does the actual work
 				System.out.print("[i: " + i + ", j: " + j + "] ");
 			}
-			//The outer loop moves to the next row
+			// The outer loop moves to the next row
 			System.out.println();
+		}
+		//lets print a grid of stars to the console
+		//***
+		//***
+		//***
+		//***
+		//***
+		//***
+		for (int i = 0; i < 5; i++) {
+
+			for (int j = 0; j < 3; j++) {
+				System.out.print("* ");
 			}
+			// The outer loop moves to the next row
+			System.out.println();
+		}
+		//print out
+		//* * * * *
+		//* * * *
+		//* * *
+		//* *
+		//*
+		for (int i = 0; i < 5; i++) {
+
+			for (int j = 0; j < 5 - i; j++) {
+				System.out.print("* ");
+			}
+			// The outer loop moves to the next row
+			System.out.println();
+		}
+		//Extra Credit 1:
+		//Flip it upside down
+		//* 
+		//* *
+		//* * *
+		//* * * *
+		//* * * * *
+		
+		//Extra Credit 2:
+		//Create a Diamond
+		//     *
+		//    * *
+		//   * * *
+		//  * * * *
+		// * * * * *
+		//  * * * *
+		//   * * *
+		//    * *
+		//     *
+		
 
 	}
 
