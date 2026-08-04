dfbd5a4184bad6642141c643fdcdd63895da5dd1
Joshua Emery <38080740+JoshuaEmery@users.noreply.github.com>
2026-08-04 11:57:20 -0700

    Update IntroToArrays.java

--- Week5/IntroToArrays.java
+++ Week5/IntroToArrays.java
@@ -57,6 +57,8 @@ public class IntroToArrays {
 		//Calling our array methods
 		int[] numbers = randomArray(20);
 		displayArray(numbers);
+		int max = findMax(numbers);
+		System.out.println(max);
 		
 		
 	}
@@ -78,13 +80,32 @@ public class IntroToArrays {
 		}
 		return result;
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-
+	//lets make a method that takes in an int[] and an int searchValue
+	//go through the array and see if searchValue exists in the array
+	//If you find it return the index where you found it
+	//If not return -1 
+	static int find(int[] array, int searchValue) {
+		for (int i = 0; i < array.length; i++) {
+			//check if the element at index i == searchValue
+			if(array[i] == searchValue) {
+				return i;
+			}
+		}
+		//what do I know logically if I make it down here?
+		return -1;
+	}
+	//create a method that takes in an int[] and returns
+	//the largest element inside of that array
+	static int findMax(int[] array) {
+		//start off with a max as the first (or last element)
+		int max = array[0];
+		//start off with i at 1 since we already have the element at 0
+		for(int i = 1; i < array.length; i++) {
+			//we check for a new max
+			if(max < array[i]) {
+				max = array[i];
+			}
+		}
+		return max;
+	}
 }
