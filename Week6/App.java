2834d4f133c3e08aa36cc2c288b2da100509f778
Joshua Emery <38080740+JoshuaEmery@users.noreply.github.com>
2026-08-11 12:19:20 -0700

    Fix vehicle year in App.java constructor

--- Week6/App.java
+++ Week6/App.java
@@ -5,17 +5,6 @@ public class App {
 		// TODO Auto-generated method stub
 		//creating a new variable, its type is Vehicle
 		//passing values for the fields to the constructor
-		Vehicle vehicle1 = new Vehicle(-1000, "Honda", "Accord", 10000);
-		System.out.println("Price before: " + vehicle1.getPrice());
-		vehicle1.setPrice(5000);
-		System.out.println("Price after: " + vehicle1.getPrice());
-		
-public class App {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//creating a new variable, its type is Vehicle
-		//passing values for the fields to the constructor
 		Vehicle vehicle1 = new Vehicle(2010, "Honda", "Accord", 10000);
 		System.out.println("Price before: " + vehicle1.getPrice());
 		vehicle1.setPrice(5000);
@@ -27,10 +16,6 @@ public class App {
 		//sysout is a static method, we never made an instance of an object first
 		//Math.pow is a static method, we never made an instance of the math object first
 		
-	}
-
-}
-
 	}
 
 }
