ca0c5e62cee4a04b1798737daecdf0004bd6131e
Joshua Emery <38080740+JoshuaEmery@users.noreply.github.com>
2026-08-11 10:26:19 -0700

    Add files via upload

---
+++ Week6/IntroTo2dArrays.java
@@ -0,0 +1,23 @@
+
+public class IntroTo2dArrays {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//A 2d array is a grid and each element has two indices
+		//row and the column
+		//Every element is the same data type
+		//the indices are always ints
+		//dataType[][] grid = new dataType[rows][columns]
+		int[][] grid = new int[3][4];
+		//insert the number 10 into the first row second column
+		grid[0][1] = 10;
+		grid[1][1] = 20;
+		
+	}
+	//lets make a method that displays a 2d int array
+	static void displayGrid(int[][] matrix) {
+		
+	}
+	
+
+}
