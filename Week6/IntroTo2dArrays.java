302ae4a7e73fb3fc208d08920633413ca7cc1329
Joshua Emery <38080740+JoshuaEmery@users.noreply.github.com>
2026-08-11 10:34:33 -0700

    Update IntroTo2dArrays.java

--- Week6/IntroTo2dArrays.java
+++ Week6/IntroTo2dArrays.java
@@ -12,11 +12,20 @@ public class IntroTo2dArrays {
 		//insert the number 10 into the first row second column
 		grid[0][1] = 10;
 		grid[1][1] = 20;
+		displayGrid(grid);
 		
 	}
 	//lets make a method that displays a 2d int array
 	static void displayGrid(int[][] matrix) {
-		
+		//matrix.length gives me the number of rows
+		//matrix[rowIndex].length i get the number of columns in that row
+		for (int i = 0; i < matrix.length; i++) {
+			//matrix[i].length gives me the number of columns in row i
+			for (int j = 0; j < matrix[i].length; j++) {
+				System.out.print("i: " + i + " j: " + j + " = " + matrix[i][j] + "   ");
+			}
+			System.out.println();
+		}
 	}
 	
 
