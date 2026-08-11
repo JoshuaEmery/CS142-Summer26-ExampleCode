80e274fdb446f6764479f3f2e92fbba82781f46d
Joshua Emery <38080740+JoshuaEmery@users.noreply.github.com>
2026-08-11 11:53:37 -0700

    Update Vehicle.java

--- Week6/Vehicle.java
+++ Week6/Vehicle.java
@@ -3,7 +3,7 @@ public class Vehicle {
 	//fields - the information you want to capture about the object
 	//should be private
 	private int year;
-	private String make;
+	private String make; 
 	private String model;
 	private double price;
 	//Constructor - special method that is called when we create
@@ -13,10 +13,57 @@ public class Vehicle {
 		//we have a new keyword here called this
 		//this refers to the instance of the class we are creating
 		//assign the fields
-		this.year = year;
+		//calling the private set price method
+		this.setYear(year);
 		this.make = make;
 		this.model = model;
+		//we can either repeat the whole validation process from the setter here
+		//or simply use the setter function in the constructor
+		this.setPrice(price);
+		}
+	//methods
+	//1. getters and setters - these are methods that control access to the fields
+	//2. actions that the class can perform
+	
+	//getters and setters - if a field should have read access it will have a getter
+	//if a field should have write access it will have a setter
+	public int getYear() {
+		return this.year;
+	}
+    //year should not be changed so we only have a getter
+	public String getModel() {
+		return this.model;
+	}
+	public String getMake() {
+		return this.make;
+	}
+	//price needs to have read and write access
+
+	public double getPrice() {
+		return this.price;
+	}
+	//setter takes in a new value for price and changes the field
+	public void setPrice(double price) {
+		//validate the price that came into the method before setting the field
+		if(price < 0) {
+			//let the user(the programmer using our class) know that something went wrong
+			//throwing an exception exits the method immediately
+			throw new IllegalArgumentException("Price cannot be negative");
+		}
 		this.price = price;
 	}
-
+	//you can make a setter than can only be used from inside the class
+	private void setYear(int year) {
+		if(year < 0) {
+			throw new IllegalArgumentException("Year cannot be negative");
+		}
+		this.year = year;
+	}
+	
+	
+	
+	
+	
+	
+	
 }
