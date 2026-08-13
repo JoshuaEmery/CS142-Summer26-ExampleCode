6f1458c6a3bfd35460c164df7f952870a06c5ff8
Joshua Emery <38080740+JoshuaEmery@users.noreply.github.com>
2026-08-13 11:57:37 -0700

    Implement distance calculation methods in Point class
    Added methods to calculate distance from origin and between two points.

--- Week6/Point.java
+++ Week6/Point.java
@@ -26,6 +26,21 @@ public class Point {
 	public void setY(double y) {
 		this.y = y;
 	}
+	//method that calculates an returns the distance from origin for a point
+	public double distanceFromOrigin() {
+		return Math.sqrt(x * x + y * y);
+	}
+	
+	//What about if we wanted a method that could calculate the distance between two points
+	//lets make it static and take in two points
+	//If you are going to use a static method for a calculation, you have to 
+	//take in everything you need as parameters. You dont have access to the fields
+	//from a static method
+	public static double distanceBetweenPoints(Point point1, Point point2) {
+		double dx = point2.getX() - point1.getX();
+		double dy = point2.getY() - point1.getY();
+		return Math.sqrt(dx * dx + dy * dy);
+	}
 
 	@Override
 	public String toString() {
