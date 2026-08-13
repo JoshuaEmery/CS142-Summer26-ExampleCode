49ad9f4d5b3d4221123661802a4197dcbee8e5d1
Joshua Emery <38080740+JoshuaEmery@users.noreply.github.com>
2026-08-13 10:44:49 -0700

    Update RevVsValue.java

--- Week6/RevVsValue.java
+++ Week6/RevVsValue.java
@@ -18,7 +18,34 @@ public class RevVsValue {
 		int[] numbers2 = numbers;
 		System.out.println(Arrays.toString(numbers));
 		System.out.println(Arrays.toString(numbers2));
+		numbers2[1] = 100;
+		System.out.println(Arrays.toString(numbers));
+		System.out.println(Arrays.toString(numbers2));
+		addToArray(numbers2);
+		System.out.println(Arrays.toString(numbers));
+		System.out.println(Arrays.toString(numbers2));
+		//This is also true for objects, objects are reference type
+		Vehicle v1 = new Vehicle(2000, "Honda", "Civic", 5000);
+		System.out.println(v1);
+		discount(v1);
+		System.out.println(v1);
+		//Strings are a weird exception, they are technically ref but behave like value
+		String input = "Hello";
+		String input2 = input;
+		System.out.println(input);
+		System.out.println(input2);
+		input = "Substation";
+		System.out.println(input);
+		System.out.println(input2);
 
+	}
+	//These methods do not return anything, but because they take reference type
+	//variables, they are able to change the objects in main()
+	static void addToArray(int[] array) {
+		array[0]++;
+	}
+	static void discount(Vehicle v) {
+		v.setPrice(1000);
 	}
 
 }
